3cbed85462fcf0a2bce29fa8e64f522993987d4a
SAHILJAIN2024 <jainsahil022@gmail.com>
2026-01-20 00:55:25 +0530

    another video update

--- frontend/src/components/VideoSection.jsx
+++ frontend/src/components/VideoSection.jsx
@@ -31,7 +31,7 @@ export default function VideoSection() {
         <div className="aspect-video">
           <video
             ref={videoRef}
-            src="public/images/Finalrecording.mp4"
+            src="/images/Finalrecording.mp4"
             muted
             loop
             playsInline
